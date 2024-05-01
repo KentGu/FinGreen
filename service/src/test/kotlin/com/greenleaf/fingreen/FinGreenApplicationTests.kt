c3c614ecad0a8140eab19f3ab6f3e7e7787b119f
Kent <kentgu2010@hotmail.com>
2024-05-01 17:10:34 +0800

    initial project

---
+++ service/src/test/kotlin/com/greenleaf/fingreen/FinGreenApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.greenleaf.fingreen
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinGreenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
